b33e01f9d75809a2eff1215035898abdeb9378cc
jalalirs <jalalirsh@hotmail.com>
2024-05-26 08:22:49 +0300

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 
 def get_version() -> str:
-    rel_path = "src/neohub/__init__.py"
+    rel_path = "src/hub/__init__.py"
     with open(rel_path, "r") as fp:
         for line in fp.read().splitlines():
             if line.startswith("__version__"):
@@ -13,10 +13,10 @@ def get_version() -> str:
 
 
 setup(
-    name="neohub",
+    name="hub",
     version=get_version(),
-    author="Saudi Aramco, Inc.",
-    author_email="jalalirs@aramco.com",
+    author="Ridwan Jalali",
+    author_email="jalalirs@gmail.com",
     description="Client library to download and publish models, datasets and other repos on neo hub",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
