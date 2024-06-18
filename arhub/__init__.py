72d98344673bdf989c5a2a057408bc32f778435d
Ridwan Jalali <jalalirsh@hotmail.com>
2024-06-17 19:51:46 -0700

    added support for specifying split

--- arhub/__init__.py
+++ arhub/__init__.py
@@ -3,9 +3,9 @@ from arhub.client import Client
 __all__ = ["Client"]
 __version__ = "0.1.0"
 
-def pull(repo_id: str, load: bool = True, download: bool = False):
+def pull(repo_id: str, load: bool = True, download: bool = False, **kwargs):
     client = Client()
-    return client.pull(repo_id, load=load, download=download)
+    return client.pull(repo_id, load=load, download=download, **kwargs)
 
 def push(data_object, repo_id):
     client = Client()
