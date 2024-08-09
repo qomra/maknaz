2840bffefc25d06b0306597c127b4a923ff26a09
Ridwan Jalali <jalalirsh@hotmail.com>
2024-08-09 14:12:08 +0300

    added missed params for push

--- arhub/__init__.py
+++ arhub/__init__.py
@@ -7,6 +7,6 @@ def pull(repo_id: str, load: bool = True, download: bool = False, **kwargs):
     client = Client()
     return client.pull(repo_id, load=load, download=download, **kwargs)
 
-def push(data_object, repo_id):
+def push(data_object, repo_id,api_path=None):
     client = Client()
-    return client.push(data_object, repo_id)
+    return client.push(data_object, repo_id,api_path=api_path)
